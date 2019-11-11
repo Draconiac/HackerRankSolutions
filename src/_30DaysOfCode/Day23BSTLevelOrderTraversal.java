c40c69c561cc566cd029d700f1bdbf95b830d902
kerem.yucel <kerem.yucel@cs.com.tr>
2019-11-11 10:54:30 +0300

    Solution

---
+++ src/_30DaysOfCode/Day23BSTLevelOrderTraversal.java
@@ -0,0 +1,83 @@
+package _30DaysOfCode;
+
+import java.util.*;
+
+class Node{
+    Node left,right;
+    int data;
+    Node(int data){
+        this.data=data;
+        left=right=null;
+    }
+}
+
+class Solution{
+
+    static void levelOrder(Node root){
+        //Write your code here
+        Queue<Node> queue = new LinkedList<>();
+        queue.add(root);
+
+        while(queue != null && queue.size() != 0){
+
+            System.out.print(queue.element().data + " ");
+            Node newRoot = queue.remove();
+
+            if(newRoot.left != null){
+                queue.add(newRoot.left);
+            }
+
+            if(newRoot.right != null){
+                queue.add(newRoot.right);
+            }
+
+        }
+
+    }
+
+    public static Node insert(Node root,int data){
+        if(root==null){
+            return new Node(data);
+        }
+        else{
+            Node cur;
+            if(data<=root.data){
+                cur=insert(root.left,data);
+                root.left=cur;
+            }
+            else{
+                cur=insert(root.right,data);
+                root.right=cur;
+            }
+            return root;
+        }
+    }
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        int T=sc.nextInt();
+        Node root=null;
+        while(T-->0){
+            int data=sc.nextInt();
+            root=insert(root,data);
+        }
+        levelOrder(root);
+    }
+}
+/*Sample
+13
+25
+39
+12
+19
+9
+23
+55
+31
+60
+35
+41
+70
+90
+
+25 12 39 9 19 31 55 23 35 41 60 70 90
+* */
