d2edfd1c6e4f4896d1e6f1e64ec3ae2271df8173
Draconiac <draconiac12@gmail.com>
2019-11-15 00:17:15 +0300

    Solution28

---
+++ src/_30DaysOfCode/Day28Regex.java
@@ -0,0 +1,72 @@
+package _30DaysOfCode;
+
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.Map;
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class Day28Regex {
+
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        int N = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        Pattern p = Pattern.compile("[a-z]{1,20} [a-z.]+@gmail\\.com{1,50}");//. represents single character
+        LinkedList<String> a = new LinkedList<>();
+
+        for (int NItr = 0; NItr < N; NItr++) {
+            String[] firstNameEmailID = scanner.nextLine().split(" ");
+
+            String firstName = firstNameEmailID[0];
+
+            String emailID = firstNameEmailID[1];
+            if(p.matcher(firstName+" "+emailID).matches()){
+                a.add(firstName);
+            }
+        }
+        scanner.close();
+
+        a.stream().sorted().forEach(System.out::println);
+
+
+    }
+
+}
+/*
+30
+riya riya@gmail.com
+julia julia@julia.me
+julia sjulia@gmail.com
+julia julia@gmail.com
+samantha samantha@gmail.com
+tanya tanya@gmail.com
+riya ariya@gmail.com
+julia bjulia@julia.me
+julia csjulia@gmail.com
+julia djulia@gmail.com
+samantha esamantha@gmail.com
+tanya ftanya@gmail.com
+riya riya@live.com
+julia julia@live.com
+julia sjulia@live.com
+julia julia@live.com
+samantha samantha@live.com
+tanya tanya@live.com
+riya ariya@live.com
+julia bjulia@live.com
+julia csjulia@live.com
+julia djulia@live.com
+samantha esamantha@live.com
+tanya ftanya@live.com
+riya gmail@riya.com
+priya priya@gmail.com
+preeti preeti@gmail.com
+alice alice@alicegmail.com
+alice alice@gmail.com
+alice gmail.alice@gmail.com
+ */
