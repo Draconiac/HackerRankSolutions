6a8c9f01076fe4008c0a38371b008192a122433d
kerem.yucel <kerem.yucel@cs.com.tr>
2019-11-18 14:18:31 +0300

    Solution 29

---
+++ src/_30DaysOfCode/Day29BitwiseAND.java
@@ -0,0 +1,55 @@
+package _30DaysOfCode;
+
+import java.util.LinkedList;
+import java.util.Scanner;
+
+public class Day29BitwiseAND {
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        int t = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        for (int tItr = 0; tItr < t; tItr++) {
+            String[] nk = scanner.nextLine().split(" ");
+
+            int n = Integer.parseInt(nk[0]);
+
+            int k = Integer.parseInt(nk[1]);
+
+            LinkedList<Integer> arr = new LinkedList<Integer>();
+            for (int i = 1; i < n; i++){
+                for (int j = i+1; j <= n; j++){
+                    if((i & j) < k) {
+                        arr.add(i & j);
+                    }
+                }
+            }
+
+            int max = -1;
+            for (Integer a : arr){
+                if(a.intValue() > max)
+                    max = a.intValue();
+            }
+
+            System.out.println(max);
+        }
+
+        scanner.close();
+    }
+}
+
+
+
+/*
+3
+5 2
+8 5
+2 2
+
+1
+4
+0
+
+ */
